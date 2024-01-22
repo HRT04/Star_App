c8482026f75e5dfa33fd7a8afce1e48e3b0aa96c
HRT04 <e215707@ie.u-ryukyu.ac.jp>
2024-01-22 21:52:57 +0900

    COMMIT3

--- src/camera.js
+++ src/camera.js
@@ -3,7 +3,7 @@ script.setAttribute("async", "");
 script.setAttribute("onload", "onOpenCvReady();");
 script.setAttribute("src", "https://docs.opencv.org/master/opencv.js");
 document.head.appendChild(script);
-console.log("commit_15");
+console.log("COMMIT3");
 
 function onOpenCvReady() {
   const cv = window.cv;
